c6d38a265cef2c71a37eb76be3c717d544615024
ltraoredr1 <ltraoredr1@gmail.com>
2026-09-16 21:54:41 +0000

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App.jsx";
+import { AppProvider } from "./contexts/AppContext.jsx";
+import { ToastProvider } from "./components/ToastContext.jsx";
+import { ErrorBoundary } from "./components/ErrorBoundary.jsx";
+import { captureRefFromUrl } from "./lib/referralApi.js";
+import "./index.css";
+
+captureRefFromUrl();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AppProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </AppProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(() => {});
+  });
+}
