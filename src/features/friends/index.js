53be5ba490a522d85f1aff0a912558fdacfa7994
ltraoredr1 <ltraoredr1@gmail.com>
2026-09-18 11:58:56 +0000

    Remove FriendRequestButton export
    Remove export of FriendRequestButton from index.js

--- src/features/friends/index.js
+++ src/features/friends/index.js
@@ -1,4 +1,3 @@
 export { FriendsTab } from "./FriendsTab.jsx";
 export { FriendRequests } from "./FriendRequests.jsx";
 export { default as FollowButton } from "./FollowButton.jsx";
-export { default as FriendRequestButton } from "./FriendRequestButton.jsx";
